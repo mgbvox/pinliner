3d43a8ce6d11628f113553895f262e1915f90243
Matthew Billman <mgbvox@gmail.com>
2024-03-27 11:59:40 -0400

    rm docs pre-migration, black, ruff, mypy

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use pinliner in a project::
-
-    import pinliner
--- pinliner/pinliner.py
+++ pinliner/pinliner.py
@@ -9,8 +9,8 @@ from pinliner import __version__
 import sys
 
 
-TEMPLATE_FILE = 'importer.template'
-TEMPLATE_PATTERN = '${CONTENTS}'
+TEMPLATE_FILE = "importer.template"
+TEMPLATE_PATTERN = "${CONTENTS}"
 
 
 def output(cfg, what, newline=True):
@@ -22,11 +22,11 @@ def output(cfg, what, newline=True):
 
 def process_file(cfg, base_dir, package_path):
     if cfg.tagging:
-        output(cfg, '<tag:' + package_path + '>')
-    path = os.path.splitext(package_path)[0].replace(os.path.sep, '.')
+        output(cfg, "<tag:" + package_path + ">")
+    path = os.path.splitext(package_path)[0].replace(os.path.sep, ".")
     package_start = cfg.outfile.tell()
     full_path = os.path.join(base_dir, package_path)
-    with open(full_path, 'r') as f:
+    with open(full_path, "r") as f:
         # Read the whole file
         code = f.read()
 
@@ -34,7 +34,7 @@ def process_file(cfg, base_dir, package_path):
         # the code as a string
         output(cfg, code.replace("'''", r"\'''"), newline=cfg.tagging)
     package_end = cfg.outfile.tell()
-    is_package = 1 if path.endswith('__init__') else 0
+    is_package = 1 if path.endswith("__init__") else 0
     if is_package:
         path = path[:-9]
 
@@ -48,10 +48,8 @@ def template(cfg):
     with open(template_path) as f:
         template = f.read()
     prefix_end = template.index(TEMPLATE_PATTERN)
-    prefix_data = template[:prefix_end].replace('%{FORCE_EXC_HOOK}',
-                                                str(cfg.set_hook))
-    prefix_data = prefix_data.replace('%{DEFAULT_PACKAGE}',
-                                      cfg.default_package)
+    prefix_data = template[:prefix_end].replace("%{FORCE_EXC_HOOK}", str(cfg.set_hook))
+    prefix_data = prefix_data.replace("%{DEFAULT_PACKAGE}", cfg.default_package)
     cfg.outfile.write(prefix_data)
     postfix_begin = prefix_end + len(TEMPLATE_PATTERN)
     return template[postfix_begin:]
@@ -87,11 +85,10 @@ def process_files(cfg):
     # modules inside the main file.
     # We don't use indent to decrease the number of bytes in the file
     data = json.dumps(inliner_packages)
-    output(cfg, 2 * os.linesep + 'inliner_packages = ', newline=False)
-    data = data.replace('],', '],' + os.linesep + '   ')
-    data = data.replace('[', '[' + os.linesep + 8 * ' ')
-    data = '%s%s    %s%s%s' % (data[0], os.linesep, data[1:-1], os.linesep,
-                               data[-1])
+    output(cfg, 2 * os.linesep + "inliner_packages = ", newline=False)
+    data = data.replace("],", "]," + os.linesep + "   ")
+    data = data.replace("[", "[" + os.linesep + 8 * " ")
+    data = "%s%s    %s%s%s" % (data[0], os.linesep, data[1:-1], os.linesep, data[-1])
 
     output(cfg, data)
     # No newline on last line, as we want output file to be PEP8 compliant.
@@ -102,12 +99,14 @@ def process_files(cfg):
 def parse_args():
     class MyParser(argparse.ArgumentParser):
         """Class to print verbose help on error."""
+
         def error(self, message):
             self.print_help()
-            sys.stderr.write('\nERROR: %s\n' % message)
+            sys.stderr.write("\nERROR: %s\n" % message)
             sys.exit(2)
 
-    general_description = """Pinliner - Python Inliner (Version %s)
+    general_description = (
+        """Pinliner - Python Inliner (Version %s)
 
     This tool allows you to merge all files that comprise a Python package into
 a single file and be able to use this single file as if it were a package.
@@ -139,47 +138,72 @@ normal package.  Modules will also behave as usual.
     By default there is no visible separation between the different modules'
 source code, but one can be enabled for clarity with option --tag, which will
 include a newline and a <tag:file_path> tag before each of the source files.
-""" % __version__
+"""
+        % __version__
+    )
     general_epilog = None
 
-    parser = MyParser(description=general_description,
-                      epilog=general_epilog, argument_default='',
-                      formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('packages', nargs='+', help='Packages to inline.')
-    parser.add_argument('--version', action='version', version=__version__)
-    parser.add_argument('-o', '--outfile', nargs='?',
-                        type=argparse.FileType('w'),
-                        default=sys.stdout, help='Output file.')
-    parser.add_argument('--set-except', default=None, dest='set_hook',
-                        action='store_true',
-                        help='Force setting handler for uncaught exceptions.')
-    parser.add_argument('--no-except', default=None, dest='set_hook',
-                        action='store_false',
-                        help="Don't set handler for uncaught exceptions.")
-    parser.add_argument('--tag', default=False, dest='tagging',
-                        action='store_true',
-                        help="Mark with <tag:file_path> each added file.")
-    parser.add_argument('-d', '--default-pkg', default=None,
-                        dest='default_package',
-                        help='Define the default package when multiple '
-                             'packages are inlined.')
+    parser = MyParser(
+        description=general_description,
+        epilog=general_epilog,
+        argument_default="",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument("packages", nargs="+", help="Packages to inline.")
+    parser.add_argument("--version", action="version", version=__version__)
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        nargs="?",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="Output file.",
+    )
+    parser.add_argument(
+        "--set-except",
+        default=None,
+        dest="set_hook",
+        action="store_true",
+        help="Force setting handler for uncaught exceptions.",
+    )
+    parser.add_argument(
+        "--no-except",
+        default=None,
+        dest="set_hook",
+        action="store_false",
+        help="Don't set handler for uncaught exceptions.",
+    )
+    parser.add_argument(
+        "--tag",
+        default=False,
+        dest="tagging",
+        action="store_true",
+        help="Mark with <tag:file_path> each added file.",
+    )
+    parser.add_argument(
+        "-d",
+        "--default-pkg",
+        default=None,
+        dest="default_package",
+        help="Define the default package when multiple " "packages are inlined.",
+    )
     cfg = parser.parse_args()
     # If user didn't pass a default package determine one ourselves.
     if cfg.default_package is None:
         # For single package file default is the package, for multiple packaged
         # files default is none (act as a bundle).
-        def_file = cfg.packages[0] if len(cfg.packages) == 1 else ''
+        def_file = cfg.packages[0] if len(cfg.packages) == 1 else ""
         cfg.default_package = def_file
     return cfg
 
 
 def is_module(module):
     # This validation is poor, but good enough for now
-    return os.path.isfile(module) and module.endswith('.py')
+    return os.path.isfile(module) and module.endswith(".py")
 
 
 def is_package(package):
-    init_file = os.path.join(package, '__init__.py')
+    init_file = os.path.join(package, "__init__.py")
     return os.path.isdir(package) and os.path.isfile(init_file)
 
 
@@ -189,15 +213,16 @@ def validate_args(cfg):
     # packages
     for package in cfg.packages:
         if not is_package(package):
-            sys.stderr.write('ERROR: %s is not a python package' % package)
+            sys.stderr.write("ERROR: %s is not a python package" % package)
             missing = True
     if missing:
         sys.exit(1)
 
     if cfg.default_package:
         if cfg.default_package not in cfg.packages:
-            sys.stderr.write('ERROR: %s is not a valid default package' %
-                             cfg.default_pkg)
+            sys.stderr.write(
+                "ERROR: %s is not a valid default package" % cfg.default_pkg
+            )
             sys.exit(2)
         # Convert the default package from path to package
         cfg.default_package = os.path.split(cfg.default_package)[1]
@@ -209,5 +234,5 @@ def main():
     process_files(cfg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
